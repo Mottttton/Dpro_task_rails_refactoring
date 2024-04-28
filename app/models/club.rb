c8a57d18983f7c72040872c71c4f15ac3566cffa
Mottttton <mottttton.webdev@gmail.com>
2024-04-28 23:30:09 +0900

    modify: refactoring

--- app/models/club.rb
+++ app/models/club.rb
@@ -29,33 +29,40 @@ class Club < ApplicationRecord
   end
 
   def win_on(year)
-    year = Date.new(year, 1, 1)
-    count = 0
-    matches.where(kicked_off_at: year.all_year).each do |match|
-      count += 1 if won?(match)
-    end
-    count
+    count_num_of("won", year)
   end
 
   def lost_on(year)
-    year = Date.new(year, 1, 1)
-    count = 0
-    matches.where(kicked_off_at: year.all_year).each do |match|
-      count += 1 if lost?(match)
-    end
-    count
+    count_num_of("lost", year)
   end
 
   def draw_on(year)
-    year = Date.new(year, 1, 1)
-    count = 0
-    matches.where(kicked_off_at: year.all_year).each do |match|
-      count += 1 if draw?(match)
-    end
-    count
+    count_num_of("draw", year)
   end
 
   def homebase
     "#{hometown}, #{country}"
+  end
+
+  def players_average_age
+    (players.sum(&:age) / players.length).to_f
+  end
+
+  private
+
+  def count_num_of(won_lost_or_draw, year)
+    year = Date.new(year, 1, 1)
+    count = 0
+    matches.where(kicked_off_at: year.all_year).each do |match|
+      case won_lost_or_draw
+      when "won" then
+        count += 1 if won?(match)
+      when "lost" then
+        count += 1 if lost?(match)
+      when "draw" then
+        count += 1 if draw?(match)
+      end
+    end
+    count
   end
 end
